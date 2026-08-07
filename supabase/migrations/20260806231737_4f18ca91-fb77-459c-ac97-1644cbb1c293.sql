5ee5b3d7820a9ace840e380898e7a64f90b9383a
Mohamed Abdalaziz <482300926@aswan1.moe.edu.eg>
2026-08-07 17:02:47 +0300

    Replace repository files

--- supabase/migrations/20260806231737_4f18ca91-fb77-459c-ac97-1644cbb1c293.sql
+++
@@ -1,6 +0,0 @@
-revoke all on function public.grant_owner_admin() from public, anon, authenticated;
-revoke all on function public.handle_new_user() from public, anon, authenticated;
-revoke all on function public.is_restricted(uuid) from public, anon;
-revoke all on function public.has_role(uuid, public.app_role) from public, anon;
-grant execute on function public.is_restricted(uuid) to authenticated;
-grant execute on function public.has_role(uuid, public.app_role) to authenticated;
